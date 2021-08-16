a9d5d12ef39c3afaf1b8cfde52c16da5441d79e0
Joezzb <50118503+Joezzb@users.noreply.github.com>
2021-08-16 22:14:54 +0800

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 15:40:03 2020
+
+@author: Lenovo
+"""
+
+
+import numpy as np
+import copy as cp
+import iisignature as sig
+
+class preprocess:
+    def __init__(self):
+        with open('F:/signature/pendigits-orig.tes','r')as f:
+            self.content_test=f.readlines()
+        with open('F:/signature/pendigits-orig.tra','r')as f:
+            self.content_train=f.readlines()        
+        #test    
+        self.data=np.empty([3498,300,2])
+        self.index=np.empty([3498])
+        self.collection=np.empty([3498,500,500])
+        self.collection_50_test=np.empty([3498,50,50])
+        self.level=2
+        self.sigcollection_test=[]
+        #train
+        self.data_train=np.empty([7494,300,2])
+        self.index_train=np.empty([7494])
+        self.sigcollection_train=[]
+        self.collection_50_train=np.empty([7494,50,50])
+    def process(self):
+        #test_process
+        n=0
+        for i in range(3498):
+            while self.content_test[n] !='.PEN_DOWN\n':
+                n+=1
+            start=cp.deepcopy(n +1)
+            num=list(self.content_test[start-3].split()[4])[1]
+            while self.content_test[n] !='.PEN_UP\n':
+                n+=1
+            end = cp.deepcopy(n-1)
+            length=end-start+1
+            temp=np.zeros([length,2])
+            for k in range(length):
+                temp[k,:]=self.content_test[start+k].split()
+            self.data[i,0:length,:]=temp
+            self.index[i]=num
+            if n ==161845:
+                n=n-4
+            while self.content_test[n+2]=='.PEN_DOWN\n':
+                start=cp.deepcopy(n+3)
+                n=n+3
+                while self.content_test[n] !='.PEN_UP\n':
+                    n+=1
+                end = cp.deepcopy(n-1)
+                length1=end-start+1
+                temp1=np.zeros([length1,2])
+                for k in range(length1):
+                    temp1[k,:]=self.content_test[start+k].split()
+                self.data[i,length,:]=['0.01','0.01']
+                self.data[i,length+1:length+1+length1,:]=temp1
+                length=length+1+length1
+        #train_process
+        n=0
+        for i in range(7494):
+            while self.content_train[n] !='.PEN_DOWN\n':
+                n+=1
+            start=cp.deepcopy(n+1)
+            num=list(self.content_train[start-3].split()[4])[1]
+            while self.content_train[n] !='.PEN_UP\n':
+                n+=1
+            end = cp.deepcopy(n-1)
+            length=end-start+1
+            temp=np.zeros([length,2])
+            for k in range(length):
+                temp[k,:]=self.content_train[start+k].split()
+            self.data_train[i,0:length,:]=temp
+            self.index_train[i]=num
+        
+            while self.content_train[n+2]=='.PEN_DOWN\n':
+                start=cp.deepcopy(n+3)
+                n=n+3
+                while self.content_train[n] !='.PEN_UP\n':
+                    n+=1
+                end = cp.deepcopy(n-1)
+                length1=end-start+1
+                temp1=np.zeros([length1,2])
+                for k in range(length1):
+                    temp1[k,:]=self.content_train[start+k].split()
+                self.data_train[i,length,:]=['0.01','0.01']
+                self.data_train[i,length+1:length+1+length1,:]=temp1
+                length=length+1+length1     
+                if n==354999:
+                    break
+    
+    def image(self):
+        for i in range(3498):
+            temp_map=np.zeros([500,500])
+            for k in range(300):
+                if self.data[i,k,0]==0.1:
+                    continue
+                if self.data[i,k,0]==0:
+                    break
+                temp_map[int(self.data[i,k,0]),int(self.data[i,k,1])]=1
+            self.collection[i,:,:]=temp_map
+            
+    def image_50(self):
+        #test
+        for i in range(3498):
+            temp_map=np.zeros([50,50])
+            for k in range(300):
+                if self.data[i,k,0]==0.01:
+                    continue
+                if self.data[i,k,0]==0:
+                    break
+                temp_map[int(self.data[i,k,0]/10),int(self.data[i,k,1]/10)]=1
+            self.collection_50_test[i,:,:]=temp_map   
+         #trian
+        for i in range(7494):
+            temp_map=np.zeros([50,50])
+            for k in range(300):
+                if self.data_train[i,k,0]==0.01:
+                    continue
+                if self.data_train[i,k,0]==0:
+                    break
+                temp_map[int(self.data_train[i,k,0]/10),int(self.data_train[i,k,1]/10)]=1
+            self.collection_50_train[i,:,:]=temp_map 
+    
+    
+    def siglayer(self,level=2):
+        self.level=level
+        depth=2*(2**self.level-1) 
+        #test
+        self.sigcollection_test=np.zeros([3498,50,50,depth])
+        for i in range(3498):
+            k=2
+            while self.data[i,k+2,0]!=0:
+                if self.data[i,k-2:k+3,0].__contains__(0.01):
+                    pass
+                else:
+                    self.sigcollection_test[i,int(self.data[i,k,0]/10),
+                                       int(self.data[i,k,1]/10),:]+=sig.sig(
+                            self.data[i,k-2:k+3,:],self.level)
+                k+=1      
+        #train
+        self.sigcollection_train=np.zeros([7494,50,50,depth])
+        for i in range(7494):
+            k=2
+            while self.data_train[i,k+2,0]!=0:
+                if self.data_train[i,k-2:k+3,0].__contains__(0.01):
+                    pass
+                else:
+                    self.sigcollection_train[i,int(self.data_train[i,k,0]/10),
+                                       int(self.data_train[i,k,1]/10),:]+=sig.sig(
+                            self.data_train[i,k-2:k+3,:],self.level)
+                k+=1       
+            
+    def siglayer_av(self,level=2):
+        self.level=level
+        depth=2*(2**self.level-1) 
+        #test
+        self.sigcollection_test=np.zeros([3498,50,50,depth])
+        for i in range(3498):
+            k=2
+            while self.data[i,k+2,0]!=0:
+                if self.data[i,k-2:k+3,0].__contains__(0.01):
+                    pass
+                else:
+                    self.sigcollection_test[i,int(self.data[i,k,0]/10),
+                                       int(self.data[i,k,1]/10),:]=sig.sig(
+                            self.data[i,k-2:k+3,:],self.level)
+                k+=1 
+        #train
+        self.sigcollection_train=np.zeros([7494,50,50,depth])
+        for i in range(7494):
+            k=2
+            while self.data_train[i,k+2,0]!=0:
+                if self.data_train[i,k-2:k+3,0].__contains__(0.01):
+                    pass
+                else:
+                    self.sigcollection_train[i,int(self.data_train[i,k,0]/10),
+                                       int(self.data_train[i,k,1]/10),:]=sig.sig(
+                            self.data_train[i,k-2:k+3,:],self.level)
+                k+=1           
+            
+if __name__=='__main__':
+    a=preprocess()
+    a.process()
+    a.image_50()            
+            
+            
+            
+            
